e704b3eac6b6e0f824b80374530001a1c887f294
ArtyomKalugin <ootsal@mail.ru>
2021-05-22 22:08:00 +0300

    Added new generation version file

---
+++ dataset/generate_csv_dataset_commonly.py
@@ -0,0 +1,54 @@
+import argparse
+import random
+
+DEFAULT_DESCRIPTION = 'CSV dataset generator script.'
+DEFAULT_SAMPLES = 100
+FILES = ["100", "500", "1000", "5000", "10000", "50000", "100000", "500000", "1000000"]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=DEFAULT_DESCRIPTION)
+
+    parser.add_argument('output',
+                        type=str,
+                        help='output CSV file, e.g. data/output.csv')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # валидация аргументов
+    if args.samples < 0:
+        raise ValueError('Number of samples must be greater than 0.')
+
+    # запись данных в файл
+
+    for elem in FILES:
+        with open(elem + ".csv", 'w') as file:
+            counter = 0
+            data = []
+
+            for n in range(int(elem)):
+                data.append([])
+
+                for h in range(int(elem)):
+                    data[n].append(0)
+
+            for i in range(int(elem) - 1, -1, -1):
+                for j in range(int(elem) - 1, len(data) - counter - 1, -1):
+                    isExist = random.randint(0, 1)
+
+                    if isExist == 1:
+                        randomNumber = random.randint(-100, 100)
+                        data[i][j] = randomNumber
+                        data[j][i] = randomNumber
+
+                counter += 1
+
+            for i in range(len(data)):
+                for j in range(len(data)):
+                    file.write(str(data[i][j]) + ",")
+
+            file.write(str(random.randint(0, int(elem))))
